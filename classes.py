7c9d7ecd2d0988777c0b91b3c03b58dc7dae3d2f
Evgeniy-ST <stoyushko.evgeniy@gmail.com>
2014-04-14 18:10:29 +0400

    Many changes in classes

--- classes.py
+++ classes.py
@@ -86,18 +86,29 @@ class Unit:
 				thStart = datetime.combine(day, self.pauses[day.weekday()].time)
 				thEnd = thStart + timedelta(seconds = (self.pauses[day.weekday()].duration*60))
 				lbad.append((thStart, thEnd))
-				if day in self.incidents:
-				        for inc in self.incidents[day]:
-				                dtStart = inc.datetime
-				                dtEnd = dtStart + timedelta(minutes = inc.duration)
-				                lbad.append((dtStart, dtEnd))
-				lbad = segment.union(lbad)
-				result = (lgood[1] - lgood[0]).total_seconds()/60
-				for l in lbad:
-				        z = segment.getintersection(lgood, l)
-				        if z:
-				                result -= (z[1] - z[0]).total_seconds()/60
-				return result
+			if day in self.incidents:
+			        for inc in self.incidents[day]:
+			                dtStart = inc.datetime
+			                dtEnd = dtStart + timedelta(minutes = inc.duration)
+			                lbad.append((dtStart, dtEnd))
+			lbad = segment.union(lbad)
+			result = (lgood[1] - lgood[0]).total_seconds()/60
+			for l in lbad:
+			        z = segment.getintersection(lgood, l)
+			        if z:
+			                result -= (z[1] - z[0]).total_seconds()/60
+			return result
+	def checkdays(self, days=None):
+		result = []
+		if not days :
+			for day in self.days:
+				result.append((day, self.checkday(day)))
+		elif type(days) == type([]):
+			for day in days:
+				result.append((day, self.checkday(day)))
+		elif type(days) == datetime or type(days) == date:
+			result.append((days, self.checkday(days)))
+		return result
 	def addday(self, addday):
 		self.days[addday.day] = addday
 	def addincident(self, incident):
@@ -108,14 +119,61 @@ class Unit:
 			self.incidents[incidentdate].append(incident)
 	def addpause(self, pause):
 		self.pauses[pause.weekday] = pause
-
-class SuperUnit(self):
-	def __init__(self):
+	def setnorm(self, norm):
+		self.norm = norm
+	def getrating(self, days):
+		daysvalue = self.checkdays(days)
+		numdays = len([y for y in daysvalue if y[1]])
+		numgooddays = len([x for x in [y[1] for y in daysvalue if y[1]] if x >= self.norm])
+		percent = round(numgooddays/numdays*100, 2)
+		message = '1' if percent>=90 else '0'
+		return self.code, self.norm, daysvalue, numdays, numgooddays, percent, message
+		
+class SuperUnit():
+	def __init__(self, code):
+		self.code = code
 		self.units = {}
-	def addunit(self, unit)
-		self.units[unit.code] = unit
-	def checkday(self, day):
-		pass
+	def __str__(self):
+		result = 'SuperUnit: ' + str(self.code) + '\n'
+		for unit in self.units:
+			result += str(self.units[unit]) + '\n'
+		return result 
+	def addunit(self, unitcode):
+		if unitcode not in self.units:
+			self.units[unitcode] = Unit(unitcode)
+	def adddaytounit(self, unitcode, day):
+		self.addunit(unitcode)
+		self.units[unitcode].addday(day)
+	def addincidenttounit(self, unitcode, incident):
+		self.addunit(unitcode)
+		self.units[unitcode].addincident(incident)
+	def addpausetounit(self, unitcode, pause):
+		self.addunit(unitcode)
+		self.units[unitcode].addpause(pause)
+	def checkdaysunit(self, unitcode, days=None):
+		if unitcode in self.units:
+			return self.units[unitcode].checkdays(days)
+	def setnormunit(self, unitcode, norm):
+		self.addunit(unitcode)
+		self.units[unitcode].setnorm(norm)
+#	def getratingunit(self, unitcode, days):
+#		if unitcode in self.units:
+#			return	(self.code,) + self.units[unitcode].getrating(days)
+	def getratingunits(self, days):
+		result = []
+		for unit in self.units:
+			if not self.units[unit].delete:
+				result.append((self.code,) + self.units[unit].getrating(days))
+		return result
+	def getratingsuperunit(self, days):
+		unitsvalue = self.getratingunits(days)
+		numdays = len(unitsvalue)
+		print(numdays)
+#		numgooddays = len([x for x in [y[1] for y in daysvalue if y[1]] if x >= self.norm])
+#		percent = round(numgooddays/numdays*100, 2)
+#		message = 'КПЭ выполнен' if percent>=90 else 'КПЭ не выполнен'
+#		return self.code, self.norm, daysvalue, numdays, numgooddays, percent, message
+		
 
 if __name__ == '__main__':
 	unit = Unit('735')
@@ -125,7 +183,28 @@ if __name__ == '__main__':
 	unit.addpause(Pause(1, time(hour = 15, minute = 0), 30))
 	unit.addincident(Incident(datetime(2014,4,1,15,25), 30))
 	unit.addincident(Incident(datetime(2014,4,1,16,25), 5))
-	print(unit)
-	print(unit.checkday(datetime(2014, 4, 1)))
+	unit.setnorm(65)
+#	print(unit)
+#	print(unit.checkday(datetime(2014, 4, 1)))
+#	print(unit.checkday(datetime(2014, 4, 2)))
+#	print(unit.checkdays([date(2014, 4, 1), date(2014, 4, 2)]))
+#	print(unit.getrating([date(2014, 4, 1), date(2014, 4, 2), date(2014, 4, 3)]))
+		
+	sunit = SuperUnit('5230')
+	sunit.adddaytounit('736', Day(date(2014, 4, 1), datetime(2014, 4, 1, 15, 0), datetime(2014, 4, 1, 17, 0)))
+	sunit.adddaytounit('736', Day(datetime(2014, 4, 2), datetime(2014, 4, 2, 15, 15), datetime(2014, 4, 2, 17, 0)))
+	sunit.addpausetounit('736', Pause(1, time(hour = 15, minute = 0), 30))
+	sunit.addincidenttounit('736', Incident(datetime(2014,4,1,15,25), 30))
+	sunit.addincidenttounit('736', Incident(datetime(2014,4,1,16,25), 5))
 
-	
+	sunit.adddaytounit('737', Day(date(2014, 4, 1), datetime(2014, 4, 1, 15, 0), datetime(2014, 4, 3, 17, 10)))
+	sunit.adddaytounit('737', Day(datetime(2014, 4, 2), datetime(2014, 4, 2, 15, 15), datetime(2014, 4, 2, 17, 0)))
+	sunit.addpausetounit('737', Pause(1, time(hour = 15, minute = 0), 30))
+	sunit.addincidenttounit('737', Incident(datetime(2014,4,1,15,25), 30))
+	sunit.addincidenttounit('737', Incident(datetime(2014,4,1,16,25), 5))
+	print(sunit)
+#	print(sunit.checkdaysunit('736'))
+	print(sunit.getratingunits([date(2014, 4, 1), date(2014, 4, 2), date(2014, 4, 3)]))
+	print(sunit.getratingsuperunit([date(2014, 4, 1), date(2014, 4, 2), date(2014, 4, 3)]))
+
+#	print(sunit)
